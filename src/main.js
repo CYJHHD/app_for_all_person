273708c67e0065569bbfb93ce57d2827e9097fbb
huanghongda <huanghongda@qq.com>
2022-12-03 19:51:57 +0800

    project auto restart optimize

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 const Koa = require('koa')
+const {APP_PORT} = require('./config/config.default')
 const app = new Koa()
 
-app.use( async ( ctx ) => {
+app.use(( ctx, next) => {
   ctx.body = 'hello koa2'
 })
 
-app.listen(3000)
-console.log('[demo] start-quick is starting at port 3000')
+app.listen(APP_PORT, () =>{
+    console.log(`server is running on http://localhost:${APP_PORT}`)
+})
