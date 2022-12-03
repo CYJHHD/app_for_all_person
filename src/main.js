c8396a1e2ed0022a8093a782d642600140ad87fb
huanghongda <huanghongda@qq.com>
2022-12-03 21:02:01 +0800

    directory structure optimization

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-const Koa = require('koa')
 const {APP_PORT} = require('./config/config.default')
-const app = new Koa()
-
-app.use(( ctx, next) => {
-  ctx.body = 'hello koa2'
-})
-
+const app = require('./app')
 app.listen(APP_PORT, () =>{
     console.log(`server is running on http://localhost:${APP_PORT}`)
 })
