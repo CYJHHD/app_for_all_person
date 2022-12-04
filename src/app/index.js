acb39206558dc975a69421a8863312dc32896272
huanghongda <huanghongda@qq.com>
2022-12-04 14:56:46 +0800

    6.body split service

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,7 @@
 const Koa = require('koa')
+const KoaBody = require('koa-body')
 const userRouter = require('../router/user.route')
 const app = new Koa()
+app.use(KoaBody())
 app.use(userRouter.routes())
 module.exports = app
