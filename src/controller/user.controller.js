385f75e75f6ba188ee978c63a45fb2a487e683e6
huanghongda <huanghongda@qq.com>
2022-12-04 22:46:20 +0800

    11. 拆分中间件

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,14 +1,22 @@
-const {createUser} = require('../service/user.service')
+const { createUser, getUerInfo } = require('../service/user.service')
 class UserController {
     async register(ctx, next) {
         // 1. 获取数据
         // console.log(ctx.request.body)
         const { user_name, password } = ctx.request.body
+
         // 2. 操作数据库
         const res = await createUser(user_name, password)
         // console.log(res)
         // 3. 返回结果
-        ctx.body = ctx.request.body
+        ctx.body = {
+            code: 0,
+            message: '用户注册成功',
+            result: {
+              id: res.id,
+              user_name: res.user_name,
+            },
+          }
     }
     async login(ctx, next){
         ctx.body = '用户登录成功'
