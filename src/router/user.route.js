f327e655dc68278b1257b7813dab456a77a942fa
huanghongda <huanghongda@qq.com>
2022-12-05 21:58:14 +0800

    13.用户登录成功

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,12 +1,12 @@
 const Router = require('koa-router')
 
-const {verifyUser, userValidator, crpytPassword} = require('../middleware/user.middleware')
+const {verifyUser, userValidator, crpytPassword, verifyLogin} = require('../middleware/user.middleware')
 const {register, login} = require('../controller/user.controller')
 
 const router = new Router({prefix:'/users'})
 
 router.post('/register', userValidator, verifyUser, crpytPassword, register)
 
-router.post('/login', login)
+router.post('/login', verifyLogin, login)
 
 module.exports = router
