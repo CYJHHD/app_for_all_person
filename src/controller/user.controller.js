c8b4eed27b6214036c1e153bd58996db1db63588
huanghongda <huanghongda@qq.com>
2022-12-05 23:00:06 +0800

    16.用户token颁发成功

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,8 @@
-const { createUser } = require('../service/user.service')
+const jwt = require('jsonwebtoken')
+
+const { createUser, getUerInfo} = require('../service/user.service')
+
+const {JWT_SECRET} = require('../config/config.default')
 
 const {userRegisterError} = require('../constant/err.type')
 class UserController {
@@ -25,9 +29,25 @@ class UserController {
             ctx.app.emit('error', userRegisterError, ctx)
         }
     }
-    async login(ctx, next){
-        ctx.body = '用户登录成功'
-    }
+    async login(ctx, next) {
+        const { user_name } = ctx.request.body
+      
+        // 1. 获取用户信息(在token的payload中, 记录id, user_name, is_admin)
+        try {
+          // 从返回结果对象中剔除password属性, 将剩下的属性放到res对象
+          const { password, ...res } = await getUerInfo({ user_name })
+      
+          ctx.body = {
+            code: 0,
+            message: '用户登录成功',
+            result: {
+              token: jwt.sign(res, JWT_SECRET, { expiresIn: '1d' }),
+            },
+          }
+        } catch (err) {
+          console.error('用户登录失败', err)
+        }
+      }
 }
 
 module.exports = new UserController()
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,6 +1,9 @@
 const Router = require('koa-router')
 
 const {verifyUser, userValidator, crpytPassword, verifyLogin} = require('../middleware/user.middleware')
+
+const {auth} = require('../middleware/auth.middleware')
+
 const {register, login} = require('../controller/user.controller')
 
 const router = new Router({prefix:'/users'})
@@ -8,5 +11,10 @@ const router = new Router({prefix:'/users'})
 router.post('/register', userValidator, verifyUser, crpytPassword, register)
 
 router.post('/login', verifyLogin, login)
+
+// router.patch('/', auth, (ctx, next) => {
+//     console.log(ctx.state.user)
+//     ctx.body = '修改密码成功'
+//   })
 
 module.exports = router
