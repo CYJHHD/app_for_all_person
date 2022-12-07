4a1d424901fc16faa226ad93e963006fe3adbd0d
huanghongda <huanghongda@qq.com>
2022-12-07 23:02:52 +0800

    20.封装管理员权限

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const { JWT_SECRET } = require('../config/config.default')
 
-const { tokenExpiredError, invalidToken } = require('../constant/err.type')
+const { tokenExpiredError, invalidToken, hasNotAdminPermission } = require('../constant/err.type')
 
 const auth = async (ctx, next) => {
     const { authorization } = ctx.request.header
@@ -28,6 +28,18 @@ const auth = async (ctx, next) => {
     await next()
   }
 
-  module.exports = {
-    auth,
+const hadAdminPermission = async (ctx, next) => {
+  const { is_admin } = ctx.state.user
+
+  if (!is_admin) {
+    console.error('该用户没有管理员的权限', ctx.state.user)
+    return ctx.app.emit('error', hasNotAdminPermission, ctx)
   }
+
+  await next()
+}
+
+module.exports = {
+  auth,
+  hadAdminPermission,
+}
--- src/router/goods.route.js
+++ src/router/goods.route.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router')
 
+const {auth , hadAdminPermission }= require('../middleware/auth.middleware')
+
 const router = new Router({ prefix: '/goods' })
 
 const {upload} = require('../controller/goods.controller')
 
-router.post('/upload', upload)
+router.post('/upload',auth , hadAdminPermission, upload)
 
 module.exports = router
