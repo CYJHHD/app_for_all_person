0b8ea381e02de488a1bf576648e684555c76d0f1
huanghongda <huanghongda@qq.com>
2022-12-07 23:55:19 +0800

    22图片上传格式优化

--- src/controller/goods.controller.js
+++ src/controller/goods.controller.js
@@ -2,13 +2,19 @@ const path = require('path')
 
 const {
     fileUploadError,
+    unSupportedFileType,
   } = require('../constant/err.type')
 
 class GoodsController {
     async upload(ctx, next) {
         const { file } = ctx.request.files
         // console.log(ctx.request.files.file)
+        const fileTypes = ['image/jpeg', 'image/png']
         if (file) {
+            if (!fileTypes.includes(file.type)) {
+                return ctx.app.emit('error', unSupportedFileType, ctx)
+                //最好用formidable 这样还是会上传只是返回的是一个错误罢了
+            }
             ctx.body = {
               code: 0,
               message: '商品图片上传成功',
