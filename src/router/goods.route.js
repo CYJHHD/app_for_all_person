118e197c99fa2a3fb862abfe4566de9d79780e71
huanghongda <huanghongda@qq.com>
2022-12-10 00:10:19 +0800

    28商品列表的实现

--- src/router/goods.route.js
+++ src/router/goods.route.js
@@ -5,7 +5,7 @@ const { validator } = require('../middleware/goods.middleware')
 
 const router = new Router({ prefix: '/goods' })
 
-const {upload, create, update, remove, restore} = require('../controller/goods.controller')
+const {upload, create, update, remove, restore, findAll} = require('../controller/goods.controller')
 
 router.post('/upload',auth , hadAdminPermission, upload)
 
@@ -18,5 +18,7 @@ router.put('/:id', auth, hadAdminPermission, validator, update)
 router.post('/:id/off', auth, hadAdminPermission, remove)
 
 router.post('/:id/on', auth, hadAdminPermission, restore)
+
+router.get('/', findAll)
 
 module.exports = router
